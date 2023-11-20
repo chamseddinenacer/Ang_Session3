bc8b2bd8c6354725c2c45db30015b388d9cbb7d7
chamseddinenacer <chamseddine.nacer@isimg.tn>
2023-11-20 13:04:30 +0100

    ACC

--- src/app/add-memeber/add-memeber.component.ts
+++ src/app/add-memeber/add-memeber.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MemberService } from '../services/member.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-memeber',
   templateUrl: './add-memeber.component.html',
   styleUrls: ['./add-memeber.component.css']
 })
-export class AddMemeberComponent {
+export class AddMemeberComponent implements OnInit {
+  members: any[] = [];
+  newMember: any = {};
+
+  constructor(private membersService:MemberService,  private router: Router) {}
+
+  ngOnInit() {
+    // this.members = this.membersService.getMemebers();
+  }
+
+  addMemeber():void {
+
+    if(this.newMember){
+    
+      this.membersService.addMemeber(this.newMember);
+      this.newMember = {}; 
+      this.members = this.membersService.getMemebers();  
+      console.log("dsdsdsdsddsd")
+
+     this.router.navigate(['/membersList']);
+    }
+    else{
+      console.log("leeeeeeeeeeeeeeeee")
+    }
+    
+      }
+
 
 }
